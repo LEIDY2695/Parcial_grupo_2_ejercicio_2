4245c3833f85c5728c4e4dc57a0251395f31f4c2
LEIDY2695 <leidyj2695@gmail.com>
2026-05-30 16:58:54 -0500

    Distancia entre dos puntos - parcial 2

---
+++ Distancia_entre_dos_puntos.py
@@ -0,0 +1,8 @@
+#Calcular la distancia entre dos puntos en el plano cartesiano
+import math
+#Pedir al usuario las coordenadas de los dos puntos
+x1 = int(input())
+y1 = int(input())
+x2 = int(input())
+y2 = int(input())
+
