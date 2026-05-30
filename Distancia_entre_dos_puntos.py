0e09b1c4653c8430bee44a628c869c9acaaadd04
LoraineCastr <loraine.castro.1214@miremington.edu.co>
2026-05-30 17:47:04 -0500

    impresion de resultado y comentarios

--- Distancia_entre_dos_puntos.py
+++ Distancia_entre_dos_puntos.py
@@ -1,3 +1,8 @@
+#Problema: Hallar la distancia entre dos puntos en el plano cartesiano,
+#con las cordenas indicadas por el usuario.
+
+#Solucion: implementar un programa que calcule la distancia entre los dos puntos con
+#la formula euclidiana: d = ((x2 - x1)^2 + (y2 - y1)^2)^0.5
 
 #Pedir al usuario las coordenadas de los dos puntos
 x1 = int(input())
@@ -6,4 +11,5 @@ x2 = int(input())
 y2 = int(input())
 
 d = ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-print(d)
+print(f"distance={d:.2f}")
+
